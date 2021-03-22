ae230d8a929e018a84c145bf58c2d9940ffaf39c
Luciano <lucianomammino@gmail.com>
2021-03-22 15:22:42 +0000

    Improved solution for ex2

--- 02-readable-streams/exercises/count-words.solution.js
+++ 02-readable-streams/exercises/count-words.solution.js
@@ -1,4 +1,6 @@
 export default async function countWords (srcStream) {
+  srcStream.setEncoding('utf8') // makes sure we process this as text (avoids multi-bytes errors)
+
   let numWords = 0
   let lastWordFromPreviousChunk = ''
 
@@ -7,6 +9,12 @@ export default async function countWords (srcStream) {
     lastWordFromPreviousChunk = words.pop()
 
     numWords += words.length
+  }
+
+  // at the end of the loop we might still have some data
+  // to process in our lastWordFromPreviousChunk
+  if (lastWordFromPreviousChunk) {
+    numWords += lastWordFromPreviousChunk.split(/\s+/).length
   }
 
   return numWords
